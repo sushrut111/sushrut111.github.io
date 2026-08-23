2a727f59abe0d262315f82e2b0dfb23c346af135
Sushrut Kasture <kasturesushrut@gmail.com>
2026-08-23 18:27:02 +0530

    Revamp portfolio landing page

--- voicecommands.js
+++ voicecommands.js
@@ -1,24 +1,16 @@
-
-    if (annyang) {
-  // Let's define our first command. First the text we expect, and then the function it should call
-  var commands = {
+if (typeof annyang !== 'undefined') {
+  const commands = {
     'scroll (to) *tag': scroll,
     'show (me) *tag': scroll,
     'go (to) *tag': scroll,
   };
 
   function scroll(argument) {
-    x = UIkit.scroll("#home");
-    x.scrollTo("#"+argument);
-
+    const target = argument.toLowerCase().replace(/\s+/g, '-');
+    const section = document.getElementById(target) ? `#${target}` : '#home';
+    UIkit.scroll(document.documentElement).scrollTo(section);
   }
-  // Add our commands to annyang
-  annyang.addCommands(commands);
 
-  // Start listening. You can call this here, or attach this call to an event, button, etc.
+  annyang.addCommands(commands);
   annyang.start();
 }
-
-
-
-
